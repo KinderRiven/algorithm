2171006379b32b5f3f4d6afa369374afc6005652
KinderRiven <1339764596@qq.com>
2017-05-23 22:42:03 +0800

    5.23

---
+++ leetcode/242.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+    }
+};
+
+int main()
+{
+
+}
---
+++ leetcode/371.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+int getSum(int a, int b) {
+    return a + b;
+}
+
+int main()
+{
+    printf("%d\n", getSum(1, 2));
+}
