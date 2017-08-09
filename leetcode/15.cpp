84a3841891f7168539bbe43e27496b03fb3685b7
K <1339764596@qq.com>
2017-08-09 15:51:09 +0800

    lt15

--- leetcode/15.cpp
+++ leetcode/15.cpp
@@ -1,4 +1,36 @@
 //
 // Created by 韩书楷 on 2017/8/9.
 //
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
+
+int main() {
+
+    vector<int> arr;
+    Solution solution;
+    int tmp;
+ 
+   while(scanf("%d", &tmp) != EOF) {
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    vector<vector<int>> res = solution.threeSum(arr);
+    for(int i = 0; i < res.size(); i++) {
+        for(int j = 0; j < res[i].size(); j++) {
+            printf("%d ", res[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
